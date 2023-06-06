bf71c9cee8f8681a93b6e39c0bf02b79de36bc7b
Cherish C. Shed <c.shed2000@gmail.com>
2023-06-06 14:15:17 +0100

    did create and get api for the notescontroller

--- Controllers/notes.controller.js
+++ Controllers/notes.controller.js
@@ -1,7 +1,35 @@
 require("dotenv").config();
-const database = require("../models/database.model");
-const User = database.User;
+const database = require("../model/database.model");
+const { User, Notes } = database
 const fs = require('fs');
 const cheerio = require("cheerio");
-const Post = database.Post;
-const Tag = database.Tag;
+
+
+const notesController = {
+    getAllNotes: async (req, res) => {
+        Notes.find({})
+            .then((err, notes) => {
+                if (err) {
+                    res.json({ error: err, status: "error" });
+                } else {
+                    res.json({ data: notes, status: "success" });
+                }
+            })
+    },
+    createNote: async (req, res) => {
+        const { title, content } = req.body;
+        const newNote = new Note({
+            title, content
+        })
+        newNote.save()
+            .then((error, result) => {
+                if (error) {
+                    res.json({ error, status: "error" })
+                } else {
+                    res.json({ data: result, status: "success" })
+                }
+            });
+    }
+}
+
+module.exports = notesController;
--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const db = require("./model/database.model")
+const db = require("./model/database.model");
+const notesController = require('./Controllers/notes.controller');
 var corsOptions = {
     origin: "*"
 }
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    const notes = await db.Notes.find();
-    res.json(notes);
-})
+app.get('/', notesController.getAllNotes);
+
+app.post('/', notesController.createNote)
 app.listen(8081, () => {
     console.log("listening on port 8081");
 })
