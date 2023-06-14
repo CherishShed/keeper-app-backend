f9fe762e7487ac1cb39271777cfc07a858fc7311
Cherish C. Shed <c.shed2000@gmail.com>
2023-06-14 11:01:23 +0100

    initialized user db

---
+++ Controllers/user.controller.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const database = require("../model/database.model");
+const { User, Notes } = database
+const fs = require('fs');
+// const cheerio = require("cheerio");
+
+
+const userController = {
+    getAllNotes: async (req, res) => {
+        const test = false;
+        if (test) {
+            res.json({ status: "noAuth", redirect: "/footer" })
+        } else {
+            Notes.find({})
+                .then((notes, err) => {
+                    if (err) {
+                        res.json({ error: err, status: "error" });
+                    } else {
+                        res.json({ data: notes, status: "success" });
+                    }
+                })
+        }
+    },
+    createNote: async (req, res) => {
+        const { title, content } = req.body;
+        const newNote = new Notes({
+            title, content
+        })
+        newNote.save()
+            .then((result, error) => {
+                if (error) {
+                    res.json({ error, status: "error" })
+                } else {
+                    res.json({ data: result, status: "success" })
+                }
+            });
+    },
+    deleteNote: async (req, res) => {
+        Notes.findByIdAndDelete(req.params.id)
+            .then((result, error) => {
+                // console.log(result);
+                if (error) {
+                    res.json({ error, status: "error" })
+                } else {
+                    res.json({ data: result, status: "success" })
+                }
+            });
+    }
+}
+
+module.exports = userController;
