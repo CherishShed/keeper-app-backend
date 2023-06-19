dc8738acb87ea80bde8ee6f163436583b91320ff
Cherish C. Shed <c.shed2000@gmail.com>
2023-06-19 09:38:42 +0100

    ADDED .ENV

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,19 +1,23 @@
-require("dotenv").config();
-const passport = require('passport');
-const database = require("../model/database.model");
-
-passport.serializeUser(function (user, cb) {
-    process.nextTick(function () {
-        // console.log(user)
-        cb(null, user);
-    });
-});
-
-passport.deserializeUser(function (user, cb) {
-    process.nextTick(function () {
-        return cb(null, user);
-    });
-});
-
-
-module.exports = passport;
+require("dotenv");
+const { User, Notes } = require("../model/database.model");
+const passport = require("passport");
+const jwt = require("jsonwebtoken")
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findById(jwt_payload.id)
+        .then((user, err) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+        });
+}));
